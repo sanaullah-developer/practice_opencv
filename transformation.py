1d1df512234592edd6dc04d44ddfcc2cd66e3c72
sanaullah-developer <sanaullahafd07@gmail.com>
2026-04-12 23:41:55 +0500

    adding another file

--- transformation.py
+++ transformation.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('../Resources/Photos/park.jpg')
+img = cv.imread('data/profile.jpg')
 cv.imshow('Park', img)
 
 # Translation
@@ -23,12 +23,13 @@ def rotate(img, angle, rotPoint=None):
     (height,width) = img.shape[:2]
 
     if rotPoint is None:
-        rotPoint = (width//2,height//2)
+        rotPoint = width//2 , height//2
     
-    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
-    dimensions = (width,height)
+    rotMat = cv.getRotationMatrix2D(rotPoint,angle,1.0)
+    dimensions = (width, height)
 
     return cv.warpAffine(img, rotMat, dimensions)
+
 
 rotated = rotate(img, -45)
 cv.imshow('Rotated', rotated)
